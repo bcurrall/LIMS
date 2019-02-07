7de834c5016d6b72bab3ff2571e7a2f043f77a16
bcurrall <benjamincurrall@gmail.com>
2019-02-07 09:45:00 -0500

    cleanup folder

--- LIMS_v2_0/LIMS/LIMS/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
-
-
-class TestPage(forms.Form):
-    post = forms.CharField()
-    test = forms.CharField()
